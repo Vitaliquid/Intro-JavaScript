ab4c638f66b5ce0396a44de7676c5e5c106511ae
Vital Liquid <97406127+Vitaliquid@users.noreply.github.com>
2022-06-11 18:19:11 -0500

    Updated intro with switches

--- intro.js
+++ intro.js
@@ -54,3 +54,61 @@ let explorer = {
 
 console.log(explorer);
 console.log(explorer.proPer);
+
+/* Flujo Condicional */
+console.log("\n********** Flujo Condicional ***********\n")
+let number1 = 4;
+let number2 = 2;
+console.log("\n********** if / else ***********\n")
+if (number1 > number2) {
+    console.log("El número 1 es mayor que el número 2")
+}
+else if( number1 == number2)
+    console.log("Los números son iguales")
+else {
+    console.log("El número 2 es mayor que número 1")
+}
+
+/* Ciclo condicional */
+console.log("\n**************** while ***************\n");
+let numberWhile = 0;
+while (numberWhile <= 12) {
+console.log(numberWhile);
+numberWhile = numberWhile + 2;
+}
+console.log("Aqui ya salió del while " + numberWhile)
+
+/* Ciclo condicional de una iteración minimo */
+console.log("\n**************** Do while ***************\n");
+let numeroDoWhile = 24;
+do {
+    numeroDoWhile = numeroDoWhile + 2;
+    console.log(numeroDoWhile);
+} while (numeroDoWhile < 20);
+console.log("Aquí sale del Do While " + numeroDoWhile);
+
+
+/* Ciclo for con iteración controlada */
+console.log("\n**************** For ***************\n");
+let numeroFor = 10
+for (let numeroFor = 0; numeroFor <= 12; numeroFor = numeroFor +1) {
+}
+console.log("Aquí salimos del for " + numeroFor);
+
+/* Opciones para evitar anidar condicionales */
+console.log("\n**************** Switch ***************\n");
+switch (prompt("¿Cómo está el clima?")) {
+    case "lluvioso":
+        console.log("No te vayas a mojar");
+        break;
+    case "soleado":
+        console.log("Ponte bloqueador");
+        break;
+    case "nublado":
+        console.log("Tapate bien");
+        break;
+    default:
+        console.log("no se como está el clima");
+        break;
+}
+console.log("Aquí salimos del Switch");
